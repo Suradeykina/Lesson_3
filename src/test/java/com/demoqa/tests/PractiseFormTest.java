3872fb25c1b798fe2347b5ec869097ea42ac7a77
suradeykina.mv <morozkosem16@gmail.com>
2022-11-24 00:14:34 +0300

    added steps

--- src/test/java/com/demoqa/tests/PractiseFormTest.java
+++ src/test/java/com/demoqa/tests/PractiseFormTest.java
@@ -5,6 +5,8 @@ import com.demoqa.pages.PractiseFormPage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
+
 public class PractiseFormTest extends TestData {
     PractiseFormPage practiseFormPage = new PractiseFormPage();
 
@@ -16,31 +18,37 @@ public class PractiseFormTest extends TestData {
 
     @Test
     void practiceForm() {
-        practiseFormPage.OpenForm()
-                .setName(firstName, lastName)
-                .setEmail(email)
-                .setGender(gender)
-                .setMobilePhone(mobilePhone)
-                .setSubjects(subject)
-                .setHobbies()
-                .setCurrentAddress(currentAddress)
-                .uploadPicture(file)
-                .setState(state)
-                .setCity(city)
-                .setDate(day, month, year)
-                .clickSubmit();
+        step ("Open registration form", () -> {
+            practiseFormPage.OpenForm();
+        });
 
-        // check results on the table
-        practiseFormPage.checkResults("Student Name", firstName)
-                .checkResults("Student Name", lastName)
-                .checkResults("Student Email", email)
-                .checkResults("Gender", gender)
-                .checkResults("Mobile", mobilePhone)
-                .checkResults("Date of Birth", day + " " + month + "," + year)
-                .checkResults("Subjects", subject)
-                .checkResults("Hobbies", hobbies)
-                .checkResults("Picture", file.getName())
-                .checkResults("Address", currentAddress)
-                .checkResults("State and City", state + " " + city);
+        step("Fill the form",()-> {
+                    practiseFormPage.setName(firstName, lastName)
+                            .setEmail(email)
+                            .setGender(gender)
+                            .setMobilePhone(mobilePhone)
+                            .setSubjects(subject)
+                            .setHobbies()
+                            .setCurrentAddress(currentAddress)
+                            .uploadPicture(file)
+                            .setState(state)
+                            .setCity(city)
+                            .setDate(day, month, year)
+                            .clickSubmit();
+                });
+
+        step("Check results on the table",()-> {
+            practiseFormPage.checkResults("Student Name", firstName)
+                    .checkResults("Student Name", lastName)
+                    .checkResults("Student Email", email)
+                    .checkResults("Gender", gender)
+                    .checkResults("Mobile", mobilePhone)
+                    .checkResults("Date of Birth", day + " " + month + "," + year)
+                    .checkResults("Subjects", subject)
+                    .checkResults("Hobbies", hobbies)
+                    .checkResults("Picture", file.getName())
+                    .checkResults("Address", currentAddress)
+                    .checkResults("State and City", state + " " + city);
+        });
     }
 }
